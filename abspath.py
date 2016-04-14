1f36172a2acabb6f602f8968a4d7026100c74e87
Akiva Miura <akiva.miura@gmail.com>
2016-04-14 17:04:56 +0900

    Added script: abspath.py, Fixed: guess-langcode.py

---
+++ abspath.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+import argparse
+import os
+import sys
+
+def printAbsPathList(filepaths):
+    print(str.join(' ', map(os.path.abspath, filepaths)))
+
+def cmdAbsPath(args):
+    parser = argparse.ArgumentParser(description='Get absolute paths of given files or directories')
+    parser.add_argument('filepaths', metavar="filepath", nargs="+", type=str, help='list of file names to get the absolute paths')
+    parsed = parser.parse_args(args)
+    printAbsPathList(parsed.filepaths)
+
+if __name__ == '__main__':
+    cmdAbsPath(sys.argv[1:])
+
