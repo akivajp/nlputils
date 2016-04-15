49f6fdc30183680bf35a3f0fde734789fc7f9d48
Akiva Miura <akiva.miura@gmail.com>
2016-04-15 16:20:50 +0900

    Added script: prompt.py

--- exec-parallel.py
+++ exec-parallel.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 import argparse
+import multiprocessing
 import os
 import sys
 
@@ -9,10 +10,14 @@ def execParallel(**args):
     print(args)
 
 def cmdExecParallel(args):
+    numCPUs = multiprocessing.cpu_count()
     parser = argparse.ArgumentParser(description='Execute command in multiple processes by splitting the target file')
     parser.add_argument('infile', type=str, help='input file name for execution')
     parser.add_argument('outfile', type=str, help='output file name for execution')
-    parser.add_argument('command', type=str, help='command line string for execution, replacing %1 and %2 with input and output files respectively')
+    parser.add_argument('command', type=str, help='command line string for execution, replacing %%1 and %%2 with input and output files respectively')
+    parser.add_argument('--splitsize', '-s', type=int, default=1000, help='assign the size (number of lines) of each splitted file')
+    parser.add_argument('--threads', '-n', type=int, default=numCPUs, help='assign the number of processes (default: %s in your computer' % numCPUs)
+    parser.add_argument('--tmpdir', '-t', type=str, default='./tmp', help='assign the path of working directory')
     parsed = parser.parse_args(args)
     execParallel(**vars(parsed))
 
---
+++ prompt.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python
+
+import argparse
+import os
+import sys
+
+def defaultString(default):
+    if default == "yes":
+        return "[Y/n]"
+    elif default == "no":
+        return "[y/N]"
+    else:
+        return "[y/n]"
+
+def getAnswer(default):
+    ans = sys.stdin.readline().strip().lower()
+    if ans in ("yes", "y"):
+        return True
+    elif ans in ("no", "n"):
+        return False
+    elif len(ans) == 0:
+        return default
+    else:
+        return None
+
+def askContinue(default=None):
+    strYN = defaultString(default)
+    print("Do you want to continue? " + strYN)
+    ans = getAnswer(default)
+    while ans == None:
+        print("Do you want to continue? " + strYN)
+        ans = getAnswer(default)
+    if ans == False:
+        sys.exit(1)
+
+def askExistContinue(filepath, default=None):
+    if os.path.exists(filepath):
+        sys.stdout.write('"%s" is found. ' % filepath)
+        askContinue(default)
+
+def cmdPrompt(args):
+    parser = argparse.ArgumentParser(description='Get absolute paths of given files or directories')
+    parser.add_argument('--exist', '-e', type=str, help='asking whether continuing when specified file already exists')
+    parser.add_argument('--continue', '-c', action='store_true', help='asking whether continuing')
+    parser.add_argument('--yes', '-y', action='store_true', help='assign the default answer as "Yes"')
+    parser.add_argument('--no', '-n', action='store_true', help='assign the default answer as "No"')
+    parsed = parser.parse_args(args)
+    default = None
+    if parsed.yes:
+        default = 'yes'
+    if parsed.no:
+        default = 'no'
+    if getattr(parsed, 'continue'):
+        return askContinue(default)
+    elif parsed.exist:
+        return askExistContinue(parsed.exist, default)
+    else:
+        sys.exit(1)
+
+if __name__ == '__main__':
+    cmdPrompt(sys.argv[1:])
+
