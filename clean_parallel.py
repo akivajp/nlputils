7c8ceb20119941a8416aebd4ddd9df218d4ef1c5
Akiva Miura <akiva.miura@gmail.com>
2016-05-14 17:39:55 +0900

    Modified: clean_paralle.py

--- clean_parallel.py
+++ clean_parallel.py
@@ -9,19 +9,20 @@ import unicodedata
 import progress
 from common import compat
 
-DEFAULT_MIN_LENGTH = 1
-DEFAULT_MAX_LENGTH = 80
-DEFAULT_RATIO = 9.0
-
 def getLongestCommonPrefix(s1, s2):
     index = 0
+    f1 = s1.split('.')
+    f2 = s2.split('.')
     while True:
-        if len(s1) <= index or len(s2) <= index:
+        if len(f1) <= index or len(f2) <= index:
             break
-        if s1[index] != s2[index]:
+        if f1[index] != f2[index]:
             break
         index += 1
-    return s1[0:index]
+    return str.join('.', f1[0:index])
+
+def getLongestCommonSuffix(s1, s2):
+    return getLongestCommonPrefix(s1[::-1],s2[::-1])[::-1]
 
 def normalize(line):
     line = line.strip()
@@ -36,21 +37,37 @@ def checkLength(lines, minLength, maxLength):
         if len(words) > maxLength: return False
     return True
 
+def getDiff(s, prefix, suffix):
+    if len(suffix) == 0:
+        return s[len(prefix):None]
+    else:
+        return s[len(prefix):-len(suffix)]
+
 def cleanParallel(**args):
-    srcFilePaths = args.get('srcfilepaths')
-    outPrefix = args.get('outfileprefix')
+    srcFilePaths = args.get('srcFilePaths')
+    #outPrefix = args.get('outfileprefix')
+    outTag = args.get('outTag')
     minLength = args.get('min')
     maxLength = args.get('max')
 
     #print(args)
-    prefix = reduce(getLongestCommonPrefix, srcFilePaths)
-    suffixes = [path[len(prefix):None] for path in srcFilePaths]
+    srcBaseNames = map(os.path.basename, srcFilePaths)
+    commonPrefix = reduce(getLongestCommonPrefix, srcBaseNames)
+    commonSuffix = reduce(getLongestCommonSuffix, srcBaseNames)
+    print(commonPrefix)
+    print(commonSuffix)
     outpaths = []
-    for suffix in suffixes:
-        filepath = outPrefix + suffix
-        if outPrefix[-1:None] == '.' and suffix[0:1] == '.':
-            filepath = outPrefix + suffix[1:None]
-        outpaths.append(filepath)
+    for path in srcFilePaths:
+        if outTag[0:1] != '.':
+            outTag = '.' + outTag
+        if commonSuffix:
+            outpath = path + outTag
+        else:
+            diff = getDiff(path, commonPrefix, commonSuffix)
+            #print(diff)
+            outpath = commonPrefix + outTag + diff
+        outpaths.append(outpath)
+    #print(outpaths)
     infiles  = [open(path,'r') for path in srcFilePaths]
     outfiles = [open(path,'w') for path in outpaths]
     counter = progress.ProgressCounter(name='Processed')
@@ -65,9 +82,12 @@ def cleanParallel(**args):
     counter.flush(force=True)
 
 def cmdCleanParallel(args):
+    DEFAULT_MIN_LENGTH = 1
+    DEFAULT_MAX_LENGTH = 80
+    DEFAULT_RATIO = 9.0
     parser = argparse.ArgumentParser(description='Clean parallel corpus by length and normalize Unicode chars')
-    parser.add_argument('srcfilepaths', metavar="filepath", nargs="+", type=str, help='path of file to clean')
-    parser.add_argument('outfileprefix', metavar="filepath_prefix", type=str, help='path of file to save')
+    parser.add_argument('srcFilePaths', metavar="filepath", nargs="+", type=str, help='path of file to clean')
+    parser.add_argument('outTag', metavar="output_tag", type=str, help='tag added in name of file to save')
     parser.add_argument('--min', default=DEFAULT_MIN_LENGTH, type=int, help='minimum #words per line')
     parser.add_argument('--max', default=DEFAULT_MAX_LENGTH, type=int, help='maximum #words per line')
     parser.add_argument('--ratio', default=DEFAULT_RATIO, type=float, help='upper bound of maximal ratio of #words between each 2 lines')
